3db9f5f65938c951aee4dedd3b0ced08bb006304
Onique Campbell <87822768+Onique-Unique@users.noreply.github.com>
2021-08-25 12:03:41 -0400

    Update Youtube _Gaming_Trending.py

--- Youtube _Gaming_Trending.py
+++ Youtube _Gaming_Trending.py
@@ -3,16 +3,10 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-
-
 web = webdriver.Chrome()
 web.get("https://www.youtube.com/feed/trending?bp=4gIcGhpnYW1pbmdfY29ycHVzX21vc3RfcG9wdWxhcg%3D%3D")
 
 time.sleep(1)
-
-#YT_TrendGameTop = web.find_element_by_id('video-title').get_attribute('aria-label')
-
-#print(YT_TrendGameTop)
 
 find_href = web.find_elements_by_id('video-title')
 for my_href in find_href:
